69096f2184cc14d1629d2cc4e3984972a6a710c8
vulyngoc <vulyngoc0306@gmail.com>
2024-06-18 06:45:09 +0700

    [Fix] can bang hinh anh

--- src/views/introduce/index.js
+++ src/views/introduce/index.js
@@ -6,47 +6,47 @@ import { useEffect, useState } from 'react';
 function Introduce() {
   return (
       <div className="body" style={{marginLeft: '4rem', marginRight: '4rem', display: 'flex', flexDirection: 'column', marginBottom: '2rem', alignItems: 'center'}}>
-        <div className='row' style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginBottom: '6rem'}}>
+        <div className='row' style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginBottom: '6rem', width: '100%'}}>
             <div className='intro-card' style={{width: '30%'}}>
                 <a href='https://www.tunhamay.vn/magazine/11-hang-gai/'>
-                <img style={{borderRadius: '0.5rem'}} src='https://www.tunhamay.vn/content/images/size/w1600/2024/03/IMG_2215.jpeg'></img>
+                <img style={{borderRadius: '0.5rem', height: '20rem !important;'}} src='https://www.tunhamay.vn/content/images/size/w1600/2024/03/IMG_2215.jpeg'></img>
                 </a>
                 <div style={{fontSize: '1.1rem', fontWeight: '400', fontFamily: 'fantasy'}}>11 Hàng Gai</div>
                 <div>Tháng 3 01, 2024 — STYLE & STORIES</div>
             </div>
             <div className='intro-card' style={{width: '30%'}}>
                 <a href='https://www.tunhamay.vn/magazine/noi-nang-ve/'>
-                    <img style={{borderRadius: '0.5rem'}} src='https://www.tunhamay.vn/content/images/size/w1200/2023/09/IMG_4217.jpeg'></img>
+                    <img style={{borderRadius: '0.5rem', height: '20rem !important;'}} src='https://www.tunhamay.vn/content/images/size/w1200/2023/09/IMG_4217.jpeg'></img>
                 </a>
                 <div style={{fontSize: '1.1rem', fontWeight: '400', fontFamily: 'fantasy'}}>11 Hàng Gai</div>
                 <div>Tháng 3 01, 2024 — STYLE & STORIES</div>
             </div>
             <div className='intro-card' style={{width: '30%'}}>
                 <a href='https://www.tunhamay.vn/magazine/viet-ve-ao-dai-ha-noi/'>
-                    <img style={{borderRadius: '0.5rem'}} src='https://www.tunhamay.vn/content/images/size/w1600/2023/09/IMG_4215.jpeg'></img>
+                    <img style={{borderRadius: '0.5rem', height: '20rem !important;'}} src='https://www.tunhamay.vn/content/images/size/w1600/2023/09/IMG_4215.jpeg'></img>
                 </a>
                 <div style={{fontSize: '1.1rem', fontWeight: '400', fontFamily: 'fantasy'}}>11 Hàng Gai</div>
                 <div>Tháng 3 01, 2024 — STYLE & STORIES</div>
             </div>
         </div>
-        <div className='row' style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginBottom: '6rem'}}>
+        <div className='row' style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginBottom: '6rem', width: '100%'}}>
             <div className='intro-card' style={{width: '30%'}}>
                 <a href='https://www.tunhamay.vn/magazine/ao-dai-tu-hoa/'>
-                    <img style={{borderRadius: '0.5rem'}} src='https://www.tunhamay.vn/content/images/size/w1600/2023/09/IMG_4213.jpeg'></img>
+                    <img style={{borderRadius: '0.5rem', height: '20rem !important;'}} src='https://www.tunhamay.vn/content/images/size/w1600/2023/09/IMG_4213.jpeg'></img>
                 </a>
                 <div style={{fontSize: '1.1rem', fontWeight: '400', fontFamily: 'fantasy'}}>11 Hàng Gai</div>
                 <div>Tháng 3 01, 2024 — STYLE & STORIES</div>
             </div>
             <div className='intro-card' style={{width: '30%'}}>
                 <a href='https://www.tunhamay.vn/magazine/sen-trang-hoi-an/'>
-                    <img style={{borderRadius: '0.5rem'}} src='https://www.tunhamay.vn/content/images/size/w1600/2023/09/DSCF4080-1-1.jpeg'></img>
+                    <img style={{borderRadius: '0.5rem', height: '20rem !important;'}} src='https://www.tunhamay.vn/content/images/size/w1600/2023/09/DSCF4080-1-1.jpeg'></img>
                 </a>
                 <div style={{fontSize: '1.1rem', fontWeight: '400', fontFamily: 'fantasy'}}>11 Hàng Gai</div>
                 <div>Tháng 3 01, 2024 — STYLE & STORIES</div>
             </div>
             <div className='intro-card' style={{width: '30%'}}>
                 <a href='https://www.tunhamay.vn/magazine/don-tet-trung-thu/'>
-                    <img style={{borderRadius: '0.5rem'}} src='https://www.tunhamay.vn/content/images/size/w1600/2023/09/dscf7007-1.jpeg'></img>
+                    <img style={{borderRadius: '0.5rem', height: '20rem !important;'}} src='https://www.tunhamay.vn/content/images/size/w1600/2023/09/dscf7007-1.jpeg'></img>
                 </a>
                 <div style={{fontSize: '1.1rem', fontWeight: '400', fontFamily: 'fantasy'}}>11 Hàng Gai</div>
                 <div>Tháng 3 01, 2024 — STYLE & STORIES</div>
