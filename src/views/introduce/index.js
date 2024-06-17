f4b01268e876d618c4b8b53e64f37373e167c20a
vulyngoc <vulyngoc0306@gmail.com>
2024-06-18 06:33:10 +0700

    [Add] map cho trang gioi thieu

--- src/views/introduce/index.js
+++ src/views/introduce/index.js
@@ -2,11 +2,10 @@ import '../../App.css';
 import './index.css'
 import { PRODUCTS } from '../../constants/product'
 import { useEffect, useState } from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 function Introduce() {
   return (
-      <div className="body" style={{marginLeft: '4rem', marginRight: '4rem', display: 'flex', flexDirection: 'column', marginBottom: '2rem'}}>
+      <div className="body" style={{marginLeft: '4rem', marginRight: '4rem', display: 'flex', flexDirection: 'column', marginBottom: '2rem', alignItems: 'center'}}>
         <div className='row' style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', marginBottom: '6rem'}}>
             <div className='intro-card' style={{width: '30%'}}>
                 <a href='https://www.tunhamay.vn/magazine/11-hang-gai/'>
@@ -53,17 +52,7 @@ function Introduce() {
                 <div>Tháng 3 01, 2024 — STYLE & STORIES</div>
             </div>
         </div>
-        <Map google={this.props.google} zoom={14}>
- 
-            <Marker onClick={this.onMarkerClick}
-                    name={'Current location'} />
-    
-            <InfoWindow onClose={this.onInfoWindowClose}>
-                <div>
-                <h1>{this.state.selectedPlace.name}</h1>
-                </div>
-            </InfoWindow>
-        </Map>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.240302901744!2d105.8296885747142!3d21.023068787975028!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab9cfc8ffddd%3A0xec338c7386d7619!2zMTM3IFAuVMO0biDEkOG7qWMgVGjhuq9uZywgVGjhu5UgUXVhbiwgxJDhu5FuZyDEkGEsIEjDoCBO4buZaSwgVmnhu4d0IE5hbQ!5e0!3m2!1svi!2s!4v1718667095792!5m2!1svi!2s" width="1000" height="450" style={{border: 0}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
       </div>
   );
 }
